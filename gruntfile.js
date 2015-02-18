837e18d729928c47756a34a536f77eee86c8e62c
12thMar <seid.adem@gmail.com>
2015-02-18 12:44:58 -0800

    grunting

--- gruntfile.js
+++ gruntfile.js
@@ -4,11 +4,20 @@ module.exports = function (grunt) {
     
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+        	options: {
+        		force: true
+        	},
+        	output:['../Source/*/obj/debug']
+        }
      
     });
 
-   // Making grunt default to force in order not to break the project.
+   // Load NPM tasks
+	require('load-grunt-tasks')(grunt);
+    // Making grunt default to force in order not to break the project.
+	
 	grunt.option('force', true);
 
-    grunt.registerTask('default', ['']);
+    grunt.registerTask('default', ['clean']);
 }
