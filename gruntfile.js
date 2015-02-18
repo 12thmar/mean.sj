c957e49be6364c7a012fd1cd7185730431a6a54f
12thMar <seid.adem@gmail.com>
2015-02-18 14:36:56 -0800

    grunt is good

--- gruntfile.js
+++ gruntfile.js
@@ -4,22 +4,17 @@ module.exports = function (grunt) {
     
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concurrent: {
-			default: ['nodemon', 'watch'],
-			debug: ['nodemon', 'watch', 'node-inspector'],
-			options: {
-				logConcurrentOutput: true,
-				limit: 10
-			}
-		}
+		clean: {
+			ouput: ['ToBeCleaned/*']
+		},
      
     });
 
    // Load NPM tasks
 	require('load-grunt-tasks')(grunt);
     // Making grunt default to force in order not to break the project.
-	
 	grunt.option('force', true);
 
-    grunt.registerTask('default', ['concurrent']);
+	grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['clean']);
 }
